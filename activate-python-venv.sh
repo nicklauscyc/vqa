a13a80f456748c0fe7dc8c49c2ef7d5f74525158
Nicklaus Choo <nicklauscyc@gmail.com>
2020-11-27 18:21:53 -0500

    enabled activation of python3 venv

---
+++ activate-python-venv.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# activate the virtual environment
+# run the command below from terminal to activate the virtual environment
+#
+# source activate-python-venv.sh
+#
+source "venv/bin/activate"
