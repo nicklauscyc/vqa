a6cf62027e20c3a247b70c2dc2fb471d0a5a3c98
Nick <ubuntu@ip-172-31-33-227.us-east-2.compute.internal>
2020-12-12 00:33:03 +0000

    updated view-log.py

--- view-log.py
+++ view-log.py
@@ -16,26 +16,43 @@ def main():
     train_acc = torch.FloatTensor(results['tracker']['train_acc'])
     train_acc = train_acc.mean(dim=1).numpy()
 
+    train_loss = torch.FloatTensor(results['tracker']['train_loss'])
+    train_loss = train_loss.mean(dim=1).numpy()
+
+    val_loss = torch.FloatTensor(results['tracker']['val_loss'])
+    val_loss = val_loss.mean(dim=1).numpy()
+
+
     # save to .csv
     with open(outputcsv + '.csv', mode='w') as out:
         outw = csv.writer(out, delimiter=',', quotechar='"',
                           quoting=csv.QUOTE_MINIMAL)
-        outw.writerow(['Epoch', 'train_acc', 'val_acc'])
+        outw.writerow(['Epoch', 'train_acc', 'val_acc', 'train_loss', 'val_loss'])
 
 
         for i in range(len(val_acc)):
-            outw.writerow(['{}'.format(i+1), train_acc[i], val_acc[i]])
+            outw.writerow(['{}'.format(i+1), train_acc[i], val_acc[i], train_loss[i],
+                           val_loss[i]])
 
 
     plt.figure()
     plt.plot(val_acc, label = "Validation Accuracy")
     plt.plot(train_acc, label = "Training Accuracy")
     plt.legend()
-    plt.title("Accuracy vs. Epoch")
+    plt.title("Plot of Accuracy against Epoch")
     plt.xlabel("Epoch")
     plt.ylabel("Accuracy")
-    plt.savefig(outputcsv + '.png')
+    plt.savefig(outputcsv + '-acc.png')
 
+    # clear figure and plot the loss
+    plt.clf()
+    plt.plot(val_loss, label = "Validation Loss")
+    plt.plot(train_loss, label = "Training Loss")
+    plt.legend()
+    plt.title("Plot of Loss against Epoch")
+    plt.xlabel("Epoch")
+    plt.ylabel("Loss")
+    plt.savefig(outputcsv + '-loss.png')
 
 if __name__ == '__main__':
     main()
